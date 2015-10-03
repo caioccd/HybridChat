503be8a0a0717a2be95d29d42bd14a7418cff7cd
kintwadi <joinemabi@gmail.com>
2015-10-03 01:36:56 -0300

    do nothing

--- HybridChatServer/src/Main.java
+++ HybridChatServer/src/Main.java
@@ -2,7 +2,7 @@ import controller.TestConnectionHandler;
 import controller.ConnectionListener;
 
 public class Main {
-	
+	//main
 	public static void main(String[] args) {
 		TestConnectionHandler testHandler = new TestConnectionHandler();
 		ConnectionListener connectionListener = new ConnectionListener(1234, testHandler);
