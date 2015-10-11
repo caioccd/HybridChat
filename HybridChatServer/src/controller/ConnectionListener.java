3c1108d7097bed09ddb39b7b8ff8e3a0253887a8
kintwadi <joinemabi@gmail.com>
2015-10-11 17:13:39 -0300

    bind client´s keepAlive to its socket

--- HybridChatServer/src/controller/ConnectionListener.java
+++ HybridChatServer/src/controller/ConnectionListener.java
@@ -4,45 +4,53 @@ import java.io.*;
 import java.net.*;
 
 public class ConnectionListener implements Runnable {
-	
-	private ServerSocket serverSocket;
-	private IConnectionHandler connectionHandler;
-	private boolean keepListening;
-	
-	public ConnectionListener(int port, IConnectionHandler connectionHandler) {
-		try {
-			this.serverSocket = new ServerSocket(port);
-			this.connectionHandler = connectionHandler;
-		}
-		catch (IOException e) {
-			System.err.printf("An error ocurred while initializing a server socket on port %d.\n", port);
-		}
-	}
 
-	public void run() {
-		keepListening = true;
-		try {
-			while (keepListening) {
-				Socket socket = serverSocket.accept();
-				connectionHandler.HandleConnection(socket.getInetAddress().getHostName(), socket.getInputStream(), socket.getOutputStream());
-				socket.close();
-			}
-		}
-		catch (IOException e) {
-			keepListening = false;
-			System.err.println("An error ocurred while accepting a connection.");
-		}
-		finally {
-			try {
-				serverSocket.close();
-			}
-			catch (IOException e) {
-				System.err.println("An error ocurred while closing a server socket.");
-			}
-		}
-	}
-	
-	public void stop() {
-		keepListening = false;
-	}
+    private ServerSocket serverSocket;
+    private IConnectionHandler connectionHandler;
+    private boolean keepListening;
+    private ObjectInputStream input;
+
+    public ConnectionListener(int port, IConnectionHandler connectionHandler) {
+        try {
+            this.serverSocket = new ServerSocket(port);
+            this.connectionHandler = connectionHandler;
+        } catch (IOException e) {
+            System.err.printf("An error ocurred while initializing a server socket on port %d.\n", port);
+        }
+    }
+
+    public void run() {
+        keepListening = true;
+        try {
+            while (keepListening) {
+
+                Socket socket = serverSocket.accept();
+
+                socket.setKeepAlive(getKeepAliveFromClientStream(socket));//bind client´s keepAlive to its socket
+
+                connectionHandler.handleConnection(socket);
+
+                socket.close();
+            }
+        } catch (IOException | ClassNotFoundException e) {
+            stop();
+            System.err.println("An error ocurred while accepting a connection.");
+        } finally {
+            try {
+                serverSocket.close();
+            } catch (IOException e) {
+                System.err.println("An error ocurred while closing a server socket.");
+            }
+        }
+    }
+
+    public void stop() {
+        keepListening = false;
+    }
+
+    //get from client a keepAlive message 
+    public boolean getKeepAliveFromClientStream(Socket socket) throws ClassNotFoundException, IOException {
+        ObjectInputStream input = new ObjectInputStream(socket.getInputStream());
+        return (boolean) input.readObject();
+    }
 }
