3c4485d9eced3e9261657390f29be375d3f90b28
kintwadi <joinemabi@gmail.com>
2015-10-11 17:19:09 -0300

    user DaoFile persistence

--- HybridChatServer/src/controller/ConnectionHandler.java
+++ HybridChatServer/src/controller/ConnectionHandler.java
@@ -1,26 +1,35 @@
 package controller;
 
 import java.io.*;
+import java.net.Socket;
+import java.net.SocketException;
+import java.util.HashMap;
 import java.util.Scanner;
-
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.User;
+import model.UserDAOFile;
 
 public class ConnectionHandler implements IConnectionHandler {
-
-	public ConnectionHandler() {
-		super();
-	}
-	
-	public void HandleConnection(String IPAddress, InputStream inputStream, OutputStream outputStream) {		
-		Scanner scanner = new Scanner(inputStream);
-		
-		while (scanner.hasNextInt()) {
-			/*switch (scanner.nextInt()) {
-			case CMD_REGISTER_USER:
-				
-				break;
-			}*/
-		}
-		
-		scanner.close();
-	}
+    User user;
+    UserDAOFile userDaoFile;
+    private final String databaseFilePath="users.txt";
+    public static  HashMap<String, User> userHashMap;
+    public ConnectionHandler() {
+        super();
+    }
+  
+    //working here
+    public void handleConnection(Socket socket) {
+        try {
+            user =new User(null, socket.getInetAddress().getHostName(),socket.getPort(), socket.getKeepAlive());
+            
+            userDaoFile=new UserDAOFile(databaseFilePath);
+            
+            userDaoFile.addUser(user);
+            
+        } catch (SocketException ex) {
+            Logger.getLogger(ConnectionHandler.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
