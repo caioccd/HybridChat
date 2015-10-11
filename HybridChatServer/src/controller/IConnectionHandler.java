058b5c81d0cd6602a9c714e378cf2a77c27c48ec
kintwadi <joinemabi@gmail.com>
2015-10-11 17:20:10 -0300

    some chenges to: IConnectionHandler

--- HybridChatServer/src/controller/IConnectionHandler.java
+++ HybridChatServer/src/controller/IConnectionHandler.java
@@ -1,9 +1,13 @@
 package controller;
 
 import java.io.*;
+import java.net.Socket;
 
 public interface IConnectionHandler {
-	
-	public void HandleConnection(String IPAddress, InputStream inputStream, OutputStream outputStream);
+
+    /*percebi que para além dos argumentos que foram antes passados 
+     faltava a porta e o nome do usuario, acabei trocando tudo por um socket para diminuir o numero de argumentos no metodo
+     */
+    public void handleConnection(Socket socket);
 
 }
