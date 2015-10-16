45da10c129a02abfeb789b1428d5ce69ad2334b1
kintwadi <joinemabi@gmail.com>
2015-10-16 11:09:20 -0300

    MainConnectionHandler

---
+++ HybridChatServer/src/controller/MainConnectionHandler.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ *
+ * @author skoop
+ */
+public class MainConnectionHandler implements IConnectionHandler{
+
+    @Override
+    public void handleConnection(String IPAddress, InputStream inputStream, OutputStream outputStream) {
+        
+    }
+    
+}
